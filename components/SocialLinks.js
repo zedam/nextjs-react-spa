7c8ec8ea23efb57288242060d681a0765cec80b5
zedam <ismael@bravoure.nl>
2018-04-17 02:19:23 +0200

    update css

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -10,27 +10,27 @@ const SocialLinks = (props) => (
 
 		{props.content.facebookLink &&
 		<Link href={props.content.facebookLink}>
-			<a className="social-links__item">Facebook.</a>
+			<a className="social-links__item" target="_blank">Facebook.</a>
 		</Link>
 		}
 		{props.content.instagramLink &&
 		<Link href={props.content.instagramLink}>
-			<a className="social-links__item">Instagram.</a>
+			<a className="social-links__item" target="_blank">Instagram.</a>
 		</Link>
 		}
 		{props.content.vimeoLink &&
 		<Link href={props.content.vimeoLink}>
-			<a className="social-links__item">Vimeo.</a>
+			<a className="social-links__item" target="_blank">Vimeo.</a>
 		</Link>
 		}
 		{props.content.linkedinLink &&
 		<Link href={props.content.linkedinLink}>
-			<a className="social-links__item">Linkedin.</a>
+			<a className="social-links__item" target="_blank">Linkedin.</a>
 		</Link>
 		}
 		{props.content.behanceLink &&
 		<Link href={props.content.behanceLink}>
-			<a className="social-links__item">Behance.</a>
+			<a className="social-links__item" target="_blank">Behance.</a>
 		</Link>
 		}
     </div>
